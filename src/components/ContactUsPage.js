43dbb3cfd2b17d59412b76110cf7f40cde12222d
Hadi Ahsan <hadi.ahsan@hotmail.com>
2024-04-23 20:49:33 -0400

    updates contact info

--- src/components/ContactUsPage.js
+++ src/components/ContactUsPage.js
@@ -8,9 +8,9 @@ function ContactUsPage() {
       <div className="contact-content">
         <div className="contact-header">
           <h2>If you have any questions or need assistance, feel free to reach out to us.</h2>
-          <p>info@blackberetinstallations.com</p>
-          <p> +1 234 567 8901</p>
-          <p>City, Country</p>
+          <p>blackberetinstallations@gmail.com</p>
+          <p>+1 647-807-6052</p>
+          <p>London, Ontario</p>
         </div>
       </div>
         <form className="contact-form" action="https://formspree.io/f/mvoeddnj" method="POST">
